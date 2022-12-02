80ced0b9aed2357cd8042f4de0771e4e60104c88
drewtato <15526875+drewtato@users.noreply.github.com>
2022-12-01 21:37:39 -0800

    Day 2 preliminary

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ These are my Advent of Code solutions written in rust. Below will be my global l
 ```txt
       -------Part 1--------   -------Part 2--------
 Day       Time  Rank  Score       Time  Rank  Score
+  2   00:10:11  3024      0   00:35:43  9700      0
   1   00:03:19  1366      0   00:05:05  1338      0
 ```
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -16,15 +16,48 @@ impl Solver for Solution {
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>) -> Self {
-		let input: Vec<i32> = file
+		let input: Vec<(u8, u8)> = file
 			.trim_ascii()
 			.lines()
-			.map(|line| line.trim_ascii().parse().unwrap())
+			.map(|line| {
+				let a = match line[0] {
+					b'A' => 0,
+					b'B' => 1,
+					b'C' => 2,
+					_ => panic!(),
+				};
+				let b = match line[2] {
+					b'X' => 0,
+					b'Y' => 1,
+					b'Z' => 2,
+					_ => panic!(),
+				};
+				(a, b)
+			})
 			.collect();
 
+		let mut total = 0;
+		for &(a, b) in &input {
+			total +=
+				(if a == b { 3 } else { 0 } + if b == (a + 1) % 3 { 6 } else { 0 } + b + 1) as i32;
+		}
+
+		let mut total2 = 0;
+		for (a, x) in input {
+			let b = match x {
+				0 => (a + 2) % 3,
+				1 => a,
+				2 => (a + 1) % 3,
+				_ => panic!(),
+			};
+			total2 +=
+				(if a == b { 3 } else { 0 } + if b == (a + 1) % 3 { 6 } else { 0 } + b + 1) as i32;
+			// println!("a: {} b: {} x: {}, total: {}", a, b, x, total2);
+		}
+
 		Self {
-			p1: "Part 1 not implemented",
-			p2: "Part 2 not implemented",
+			p1: total,
+			p2: total2,
 		}
 	}
 

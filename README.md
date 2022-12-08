e6d727bc88dc2459c81ddbb393a45b99822a8c3d
drewtato <15526875+drewtato@users.noreply.github.com>
2022-12-07 21:30:48 -0800

    Day 8 preliminary

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ These are my Advent of Code solutions written in rust. Below will be my global l
 ```sql
       --------Part 1--------   --------Part 2--------
 Day       Time   Rank  Score       Time   Rank  Score
+  8   00:19:30   3033      0   00:27:50   1854      0
   7   00:55:51   5521      0   01:02:17   4990      0
   6   00:04:55   1954      0   00:05:59   1777      0
   5   19:03:01  83782      0   19:05:37  81478      0
--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,9 +1,7 @@
-#![allow(unused)]
-
 use crate::helpers::*;
 
-type A1 = impl std::fmt::Display + std::fmt::Debug + Clone;
-type A2 = impl std::fmt::Display + std::fmt::Debug + Clone;
+type A1 = i32;
+type A2 = i32;
 
 #[derive(Debug)]
 pub struct Solution {
@@ -16,24 +14,60 @@ impl Solver for Solution {
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>) -> Self {
-		let input: Vec<i32> = file
-			.trim_ascii()
-			.lines()
-			.map(|line| line.trim_ascii().parse().unwrap())
-			.collect();
+		let input = file.trim_ascii_end().grid(|b| b - b'0');
+		let mut visible_from_edge = 0;
+		let mut best_scenic_score = 0;
+
+		for (y, row) in input.iter().enumerate() {
+			for (x, &tree) in row.iter().enumerate() {
+				let north_trees = input.iter().take(y).rev().map(|row| row[x]).collect_vec();
+				let south_trees = input.iter().skip(y + 1).map(|row| row[x]).collect_vec();
+				let west_trees = row.iter().take(x).rev().copied().collect_vec();
+				let east_trees = row.iter().skip(x + 1).copied().collect_vec();
+				// println!("{north_trees:?}");
+				// println!("{south_trees:?}");
+				// println!("{west_trees:?}");
+				// println!("{east_trees:?}");
+				'a: for range in [&north_trees, &south_trees, &west_trees, &east_trees] {
+					for &obstruction in range {
+						if obstruction >= tree {
+							continue 'a;
+						}
+					}
+					visible_from_edge += 1;
+					break;
+				}
+				let mut scenic_score = 1;
+				for range in [&north_trees, &south_trees, &west_trees, &east_trees] {
+					let mut visible_from_tree = 0;
+					for (i, &obstruction) in range.iter().enumerate() {
+						if obstruction >= tree {
+							visible_from_tree = i as i32 + 1;
+							break;
+						}
+					}
+					if visible_from_tree == 0 {
+						scenic_score *= range.len() as i32;
+					} else {
+						scenic_score *= visible_from_tree;
+					}
+				}
+				best_scenic_score = best_scenic_score.max(scenic_score);
+			}
+		}
 
 		Self {
-			p1: "Part 1 not implemented",
-			p2: "Part 2 not implemented",
+			p1: visible_from_edge,
+			p2: best_scenic_score,
 		}
 	}
 
 	fn part_one(&mut self) -> Self::AnswerOne {
-		self.p1.clone()
+		self.p1
 	}
 
 	fn part_two(&mut self) -> Self::AnswerTwo {
-		self.p2.clone()
+		self.p2
 	}
 
 	fn run_any_write<W: std::fmt::Write>(&mut self, part: u32, _writer: W) -> Res<()> {

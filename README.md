2e9dfb020d053b4854ee8e08b1a5291591add648
drewtato <15526875+drewtato@users.noreply.github.com>
2022-12-10 23:13:39 -0800

    Day 11, really slow. Can maybe get it under 1ms at the best.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ These are my Advent of Code solutions written in rust. Below will be my global l
 ```sql
       --------Part 1--------   --------Part 2--------
 Day       Time   Rank  Score       Time   Rank  Score
+ 11   01:12:40   6977      0   01:18:13   3784      0
  10   00:18:31   3566      0   00:31:33   2455      0
   9   00:30:45   4157      0   00:40:22   2394      0
   8   00:19:30   3033      0   00:27:50   1854      0
--- src/days/day11.rs
+++ src/days/day11.rs
@@ -2,13 +2,12 @@
 
 use crate::helpers::*;
 
-type A1 = impl std::fmt::Display + std::fmt::Debug + Clone;
-type A2 = impl std::fmt::Display + std::fmt::Debug + Clone;
+type A1 = u64;
+type A2 = A1;
 
 #[derive(Debug)]
 pub struct Solution {
-	p1: A1,
-	p2: A2,
+	monkeys: Vec<Monkey>,
 }
 
 impl Solver for Solution {
@@ -16,30 +15,224 @@ impl Solver for Solution {
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>) -> Self {
-		let input: Vec<i32> = file
+		let mut monkeys = file
 			.trim_ascii()
 			.lines()
-			.map(|line| line.trim_ascii().parse().unwrap())
-			.collect();
+			.map(|line| {
+				line.trim_ascii()
+					.split(|&b| matches!(b, b' ' | b','))
+					.filter_map(|word| {
+						if word.is_empty() {
+							return None;
+						}
+						Some(
+							word.wrap(std::str::from_utf8)
+								.unwrap()
+								.parse::<A1>()
+								.map_err(|_| word),
+						)
+					})
+					.collect_vec()
+			})
+			.chunks(7)
+			.into_iter()
+			.map(|chunk| {
+				let [_, l1, l2, l3, l4, l5] = chunk.array_chunks().next().unwrap();
+				let l1 = l1
+					.into_iter()
+					.skip(2)
+					.map(|word| word.unwrap())
+					.collect_vec();
+				let l2 = (l2[3].into(), l2[4].unwrap_err().into(), l2[5].into());
+				let l3 = l3.last().unwrap().unwrap();
+				let l4 = l4.last().unwrap().unwrap() as usize;
+				let l5 = l5.last().unwrap().unwrap() as usize;
+				Monkey {
+					items: l1,
+					operation: l2,
+					test: l3,
+					if_true: l4,
+					if_false: l5,
+					inspected_count: 0,
+				}
+			})
+			.collect_vec();
 
-		Self {
-			p1: "Part 1 not implemented",
-			p2: "Part 2 not implemented",
-		}
+		// dbg!(&monkeys);
+
+		Self { monkeys }
 	}
 
 	fn part_one(&mut self) -> Self::AnswerOne {
-		self.p1.clone()
+		let mut monkeys = self.monkeys.clone();
+		let mut current_monkey = 0;
+		let mut round = 0;
+		let len = monkeys.len();
+
+		loop {
+			let &mut Monkey {
+				ref mut items,
+				operation,
+				test,
+				if_true,
+				if_false,
+				ref mut inspected_count,
+			} = &mut monkeys[current_monkey % len];
+
+			*inspected_count += items.len();
+			for item in std::mem::take(items).into_iter() {
+				let op1 = match operation.0 {
+					Operand::Number(n) => n,
+					Operand::Old => item,
+				};
+				let op2 = match operation.2 {
+					Operand::Number(n) => n,
+					Operand::Old => item,
+				};
+				let acc = match operation.1 {
+					Operator::Multiply => op1 * op2,
+					Operator::Add => op1 + op2,
+				} / 3;
+
+				let dest = if acc % test == 0 { if_true } else { if_false };
+				monkeys[dest].items.push(acc);
+			}
+
+			current_monkey += 1;
+			if current_monkey == len * 20 {
+				break;
+			}
+		}
+
+		monkeys
+			.iter()
+			.map(|monkey| monkey.inspected_count as A1)
+			.sorted_unstable()
+			.rev()
+			.take(2)
+			.product()
 	}
 
 	fn part_two(&mut self) -> Self::AnswerTwo {
-		self.p2.clone()
+		let mut monkeys = self.monkeys.clone();
+		let mut current_monkey = 0;
+		let mut round = 0;
+		let len = monkeys.len();
+
+		let modulo: A1 = monkeys.iter().map(|monkey| monkey.test).product();
+
+		// For testing cycles. It's not viable since it seems like my input is unique for repeating
+		// before 10,000 rounds.
+		// let mut set = HashSet::with_capacity(100);
+		// let mut duplicate = 0;
+
+		loop {
+			let &mut Monkey {
+				ref mut items,
+				operation,
+				test,
+				if_true,
+				if_false,
+				ref mut inspected_count,
+			} = &mut monkeys[current_monkey % len];
+
+			*inspected_count += items.len();
+			let mut temp_items = std::mem::take(items);
+			for item in temp_items.drain(..) {
+				let op1 = match operation.0 {
+					Operand::Number(n) => n,
+					Operand::Old => item,
+				};
+				let op2 = match operation.2 {
+					Operand::Number(n) => n,
+					Operand::Old => item,
+				};
+				let acc = match operation.1 {
+					Operator::Multiply => op1 * op2,
+					Operator::Add => op1 + op2,
+				} % modulo;
+
+				let dest = if acc % test == 0 { if_true } else { if_false };
+				monkeys[dest].items.push(acc);
+			}
+
+			monkeys[current_monkey % len].items = temp_items;
+
+			// More cycle testing code
+			// if current_monkey % len == 0
+			// 	&& !set.insert(monkeys.iter().map(|m| m.items.clone()).collect_vec())
+			// {
+			// 	println!(
+			// 		"Got duplicate after monkey {current_monkey} on round {}",
+			// 		current_monkey / len
+			// 	);
+			// 	duplicate += 1;
+			// 	if duplicate == 5 {
+			// 		return 0;
+			// 	}
+			// }
+
+			current_monkey += 1;
+			if current_monkey == len * 10_000 {
+				break;
+			}
+		}
+
+		monkeys
+			.iter()
+			.map(|monkey| monkey.inspected_count as A1)
+			.sorted_unstable()
+			.rev()
+			.take(2)
+			.product()
 	}
 
 	fn run_any_write<W: std::fmt::Write>(&mut self, part: u32, _writer: W) -> Res<()> {
 		#[allow(clippy::match_single_binding)]
 		match part {
 			_ => Err(AocError::PartNotFound),
+		}
+	}
+}
+
+#[derive(Debug, Clone)]
+struct Monkey {
+	items: Vec<A1>,
+	operation: (Operand, Operator, Operand),
+	test: A1,
+	if_true: usize,
+	if_false: usize,
+	inspected_count: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operand {
+	Number(A1),
+	Old,
+}
+
+impl From<Result<A1, &[u8]>> for Operand {
+	fn from(value: Result<A1, &[u8]>) -> Self {
+		match value {
+			Ok(n) => Self::Number(n),
+			Err(b"old") => Self::Old,
+			_ => panic!(),
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operator {
+	Multiply,
+	Add,
+}
+
+impl From<&[u8]> for Operator {
+	fn from(value: &[u8]) -> Self {
+		match value {
+			b"+" => Self::Add,
+			b"*" => Self::Multiply,
+			_ => panic!(),
 		}
 	}
 }

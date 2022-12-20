a540e791e28d62493ec3443840f233dc4e994e27
drewtato <15526875+drewtato@users.noreply.github.com>
2022-12-19 22:56:54 -0800

    Day 20 p2 is done but I ended up breaking p1

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
 
+use std::ops::Deref;
+
 use crate::helpers::*;
 
-type A1 = impl std::fmt::Display + std::fmt::Debug + Clone;
-type A2 = impl std::fmt::Display + std::fmt::Debug + Clone;
+type A1 = i64;
+type A2 = i64;
 
 #[derive(Debug)]
 pub struct Solution {
@@ -11,54 +13,63 @@ pub struct Solution {
 	p2: A2,
 }
 
+// const OFFSET: i32 = 1_000_000;
+const OFFSET: i64 = 811_589_153;
+const MASK: u64 = 0x00_00_00_00_ff_ff_ff_ff;
+const SHIFT: u32 = 32;
+
 impl Solver for Solution {
 	type AnswerOne = A1;
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>, _: u8) -> Self {
-		let mut input: Vec<i32> = file
+		let mut input: Vec<u64> = file
 			.trim_ascii()
 			.lines()
-			.map(|line| line.parse().unwrap())
+			.enumerate()
+			.map(|(i, line)| {
+				let n = line.parse::<i32>().unwrap() as u32 as u64;
+				n | ((i as u64) << SHIFT)
+			})
 			.collect();
 
-		let queue = input.clone();
+		mix_once(&mut input);
 
-		// dbg!(input.iter().min().unwrap());
-		let len = input.len() - 1;
+		let zero_pos = input.iter().position(|&d| d & MASK == 0).unwrap();
 
-		for n in queue {
-			let mut pos = input.iter().position(|&d| d == n).unwrap();
-			input.remove(pos);
-			let index = if n >= 0 {
-				(pos + len * 10 + n as usize) % len
-			} else {
-				(pos + len * 10 - (-n as usize)) % len
-			};
-			input.insert(index, n);
-			// dbg_small!(&input);
+		let a = input[(1000 + zero_pos) % input.len()] as u32 as i32 as i64;
+		let b = input[(2000 + zero_pos) % input.len()] as u32 as i32 as i64;
+		let c = input[(3000 + zero_pos) % input.len()] as u32 as i32 as i64;
+		dbg_small!(a, b, c);
+		for &n in &input {
+			print!("{}, ", (n & MASK) as u32 as i32);
+		}
+		println!();
+
+		for _ in 0..9 {
+			mix_once(&mut input);
 		}
 
-		let zero_pos = input.iter().position(|&d| d == 0).unwrap();
+		let zero_pos = input.iter().position(|&d| d & MASK == 0).unwrap();
 
-		let &a = input.iter().cycle().nth(1000 + zero_pos).unwrap();
-		let &b = input.iter().cycle().nth(2000 + zero_pos).unwrap();
-		let &c = input.iter().cycle().nth(3000 + zero_pos).unwrap();
+		let a2 = input[(1000 + zero_pos) % input.len()] as u32 as i32 as i64 * OFFSET;
+		let b2 = input[(2000 + zero_pos) % input.len()] as u32 as i32 as i64 * OFFSET;
+		let c2 = input[(3000 + zero_pos) % input.len()] as u32 as i32 as i64 * OFFSET;
 
-		dbg_small!(a, b, c);
+		// dbg_small!(a, b, c);
 
 		Self {
 			p1: a + b + c,
-			p2: "Part 2 not implemented",
+			p2: a2 + b2 + c2,
 		}
 	}
 
 	fn part_one(&mut self, _: u8) -> Self::AnswerOne {
-		self.p1.clone()
+		self.p1
 	}
 
 	fn part_two(&mut self, _: u8) -> Self::AnswerTwo {
-		self.p2.clone()
+		self.p2
 	}
 
 	fn run_any<W: std::fmt::Write>(
@@ -71,5 +82,31 @@ impl Solver for Solution {
 		match part {
 			_ => Err(AocError::PartNotFound),
 		}
+	}
+}
+
+fn mix_once(input: &mut Vec<u64>) {
+	let len = input.len() - 1;
+
+	for i in 0..(input.len() as u64) {
+		for &n in &*input {
+			print!("{}, ", (n & MASK) as u32 as i32);
+		}
+		println!();
+		// dbg_small!(&input, i);
+		let mut pos = input
+			.iter()
+			.position(|&d| {
+				let index = d >> SHIFT;
+				index == i
+			})
+			.unwrap();
+		dbg!(pos);
+		let real_n = (input[pos] & MASK) as u32 as i32 as i64;
+		let n = real_n * OFFSET;
+		let old = input.remove(pos);
+
+		let index = (pos as i64 + n).rem_euclid(len as i64) as usize;
+		input.insert(index, old);
 	}
 }
